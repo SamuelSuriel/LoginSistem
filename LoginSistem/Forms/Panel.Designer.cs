7b9937d509fa26b6a13ee6a658b7bb40854ed0b0
Samuel Ramirez <samuelramirezsuriel@outlook.com>
2023-09-17 17:20:58 -0400

    adding panel, roundbutton

---
+++ LoginSistem/Forms/Panel.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LoginSistem.Forms
+{
+    partial class Panel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Panel));
+            panel1 = new System.Windows.Forms.Panel();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            roundButton1 = new Clases.RoundButton();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(roundButton1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(205, 734);
+            panel1.TabIndex = 0;
+            // 
+            // roundButton1
+            // 
+            roundButton1.BackColor = Color.CornflowerBlue;
+            roundButton1.FlatStyle = FlatStyle.Popup;
+            roundButton1.ForeColor = Color.White;
+            roundButton1.Image = (Image)resources.GetObject("roundButton1.Image");
+            roundButton1.Location = new Point(51, 12);
+            roundButton1.Name = "roundButton1";
+            roundButton1.Size = new Size(83, 77);
+            roundButton1.TabIndex = 2;
+            roundButton1.UseVisualStyleBackColor = false;
+            // 
+            // Panel
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1012, 734);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Panel";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Panel";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Clases.RoundButton roundButton1;
+    }
+}
